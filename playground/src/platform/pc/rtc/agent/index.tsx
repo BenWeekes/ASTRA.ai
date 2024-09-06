95c3577246bc7e1863283a8be00198f16f1da0fa
Ben Weeks <ben@agora.io>
2024-09-06 11:52:14 +0100

    o

--- playground/src/platform/pc/rtc/agent/index.tsx
+++ playground/src/platform/pc/rtc/agent/index.tsx
@@ -93,7 +93,7 @@ const Agent = (props: AgentProps) => {
         width="100%"
         height="100%"
         sttSource=""
-        ttsEnabled="false"
+        ttsEnabled={false}
       >
       </TrulienceAvatar>
     </div>
