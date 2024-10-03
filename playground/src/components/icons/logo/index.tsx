7e25c7170559fbd94190ff0a1e2a30c00f9a2790
BenWeekes <benweekes73@gmail.com>
2024-10-03 15:01:53 +0530

    feat: add AOT logo

--- playground/src/components/icons/logo/index.tsx
+++ playground/src/components/icons/logo/index.tsx
@@ -1,8 +1,17 @@
 import { IconProps } from "../types"
-import LogoSvg from "@/assets/logo.svg"
-import SmallLogoSvg from "@/assets/logo_small.svg"
+import AOTLog from "@/assets/aot-logo.png"
+import Image from "next/image"
 
 export const LogoIcon = (props: IconProps) => {
-  const { size = "default" } = props
-  return size == "small" ? <SmallLogoSvg {...props}></SmallLogoSvg> : <LogoSvg {...props}></LogoSvg>
-}
+  const { size = "default" } = props;
+  return (
+    <Image
+      src={AOTLog}
+      alt="Logo"
+      style={{
+        height: size == "small" ? "36px" : "42px",
+        width: "auto",
+      }}
+    />
+  );
+};
